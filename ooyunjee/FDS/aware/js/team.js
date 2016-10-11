13d594a4d3d5dbb84c98cc3b3d9b73e9d456e383
heoyunjee <heoyunjee@gmail.com>
2016-10-12 02:21:57 +0900

    Refactor Team Activity Aware
    Add grid toggle button

--- ooyunjee/FDS/aware/js/team.js
+++ ooyunjee/FDS/aware/js/team.js
@@ -1,8 +1,15 @@
-// function showGuide () {
-//   var body = document.body;
-//   var currentClass = body.getAttribute('class');
-//
-//   body.setAttribute('class', currentClass + ' guide');
-// }
-//
-// document.onclick = showGuide;
+var container = document.querySelector('.container');
+var toggle_grid = document.querySelector('.btn-toggle-grid');
+
+function toggleGrid() {
+  var current_class = container.getAttribute('class');
+
+  if(current_class.indexOf('show-grid') == -1) {
+    container.setAttribute('class', current_class + ' show-grid');
+  } else {
+    var removed_class = current_class.replace('show-grid', ' ').trim();
+    container.setAttribute('class', removed_class);
+  }
+}
+
+toggle_grid.onclick = toggleGrid;
