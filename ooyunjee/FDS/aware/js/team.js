8b094b5b8cc7a7a40da7984a7d3fe5af324ddc2d
heoyunjee <heoyunjee@gmail.com>
2016-10-11 11:22:14 +0900

    Add project Aware

---
+++ ooyunjee/FDS/aware/js/team.js
@@ -0,0 +1,8 @@
+// function showGuide () {
+//   var body = document.body;
+//   var currentClass = body.getAttribute('class');
+//
+//   body.setAttribute('class', currentClass + ' guide');
+// }
+//
+// document.onclick = showGuide;
